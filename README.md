81d704d5d0f4fba97c31411d3072f23953f393d8
Marc Foley <m.foley.88@gmail.com>
2016-07-26 21:29:29 +0200

    Added ttx sources for both glyphsapp 2.3 and fontmake

---
+++ README.md
@@ -0,0 +1,11 @@
+# Fontmake vs Glyphsapp for ttf generation
+
+Research is being conducted on behalf of the Google Fonts project. 
+
+A high level comparison document is available here. Please note: fontmake is still under active development. The document will be updated as fontmake improves.
+
+
+## Generate ttx files
+    gen_ttx.sh
+
+Outputs generated ttf's and ttx files into fontmake_gen and glyphs_gen, depending on which generation approach was used.
---
+++ gen_ttx.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Generate fonts with fontmake
+cd ./fontmake_gen/
+
+# Delete old ttx files and fontmake gens
+for file in *.ttx; do
+    rm "$file"
+done
+rm -r instance_ttf/ instance_ufo/ master_ufo
+
+
+fontmake -g '../Work-Sans/sources/Work Sans.glyphs' -o ttf -i --production-name
+
+# Convert fontmake ttf's into ttx
+for file in ./instance_ttf/*.ttf; do
+        ttx -d . "$file"
+done
+
+cd ../glyphs_gen/
+# Delete old ttx files 
+for file in *.ttx; do
+    rm "$file"
+done
+
+# Convert glyphs generated ttf into ttx
+# These fonts had to be generated manually in Glyphs 2.0, due to lack of headless version of app
+for file in ./instance_ttf/*.ttf; do
+        ttx -d . "$file"
+done
